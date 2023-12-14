d9115297837094d7319c30ea2d06a6115a166ecc
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-12-14 19:13:45 +0600

    Created AuthContext for firebase authentication

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routers';
+import AuthProvider from './Components/Providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
