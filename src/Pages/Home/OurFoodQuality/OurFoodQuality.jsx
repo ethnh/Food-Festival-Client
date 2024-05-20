d75ea32988c4d04388635c66dbdfa19de26b91dc
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-05-20 22:43:50 +0600

    Order from menu section updated

--- src/Pages/Home/OurFoodQuality/OurFoodQuality.jsx
+++ src/Pages/Home/OurFoodQuality/OurFoodQuality.jsx
@@ -1,24 +1,34 @@
 import { Link } from 'react-router-dom';
-import img1 from '../../../assets/img/sideImg.jpg'
-import img2 from '../../../assets/img/smImg1.jpg'
 
 const OurFoodQuality = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-                <div className='lg:w-1/2 relative'>
-                    <img src={img1} className="w-3/4 rounded-lg shadow-2xl" />
-                    <img src={img2} className='w-1/2 absolute right-5 top-1/2 border-8  border-white rounded-lg shadow-2xl' alt="" />
-                </div>
-                <div className='lg:w-1/2 space-y-5 p-4'>
-                    <h3 className='text-orange-600 text-2xl font-bold'>About Our Food Quality . . . . </h3>
-                    <h1 className="text-3xl font-bold">Elevate Your Tastebuds with Our Unparalleled Food Quality</h1>
-                    <div className="py-3">Welcome to Food Fastival! Indulge in a culinary journey where exquisite flavors and impeccable quality converge.
-                        <p className='my-2'>Our restaurant promises a delectable experience with meticulously crafted dishes using only the finest ingredients. Savor the excellence in every bite, as we redefine your dining expectations with our commitment to exceptional food quality</p>
+        <div>
+            <section
+                className="overflow-hidden lg:pt-14 md:pt-10 pt-6  sm:grid sm:grid-cols-2 sm:items-center"
+            >
+                <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+                    <div
+                        className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right"
+                    >
+                        <h3 className='text-orange-600 text-2xl font-bold'>About Our Food Quality . . . . </h3>
+                        <h1 className="text-3xl font-bold">Elevate Your Tastebuds with Our Unparalleled Food Quality</h1>
+                        <div className="py-3">Welcome to Food Fastival! Indulge in a culinary journey where exquisite flavors and impeccable quality converge.
+                            <p className='my-2'>Our restaurant promises a delectable experience with meticulously crafted dishes using only the finest ingredients. Savor the excellence in every bite, as we redefine your dining expectations with our commitment to exceptional food quality</p>
+                        </div>
+                        <Link to='/menu'><button className="px-4 py-2 bg-[#FF4860] rounded-2xl btn-outline text-white font-bold">Order Now From Menu</button></Link>
                     </div>
-                    <Link to='/menu'><button className="px-4 py-2 bg-[#FF4860] rounded-2xl btn-outline text-white font-bold">Order Now From Menu</button></Link>
                 </div>
+
+                <img
+                    alt="Violin"
+                    src="https://i.ibb.co/PQK78f1/ivan-torres-MQUqbmsz-GGM-unsplash-1.jpg"
+                    className="h-full rounded-md md:rounded-l-full  w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-ss-[30px] md:h-[calc(100%_-_4rem)] md:rounded-ss-[60px]"
+                />
+            </section >
+            <div className='mt-28 mb-28'>
+
             </div>
+
         </div>
     );
 };
